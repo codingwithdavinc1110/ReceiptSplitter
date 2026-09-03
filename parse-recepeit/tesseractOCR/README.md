30fc0005dddaa2082e8e9c8ed556383d6180e05f
CodeDaVinci <brandon.mondoy@gmail.com>
2026-09-03 19:23:10 +0200

    Aggiunto il pre-processing delle immagini
    Aggiunto per vedere il funzionamento del progetto. C'è da studiare meglio la questione per risovlere il problema con più precisione

--- parse-recepeit/tesseractOCR/README.md
+++ parse-recepeit/tesseractOCR/README.md
@@ -2,3 +2,19 @@
 # Cose da sapere prima di mettersi ad aggeggiare con il progetto
 
 pip install -r requirements.txt
+
+
+# Checklist 
+
+# Appunti pratici
+
+Per quanto riguarda tesseractOne, la prima versione, il risultato è abbastanza decente. Sicuramente, non so se per il font, o per il formato dell'immagine, non riesce a capire le parole. Ha comunque una lettura decente. Non è ancora testato ancora su scontrino vero (forse viene meglio?)
+
+
+- [x] Testato su immagine fake, funziona decente.
+- [ ] Testare su immagini vere 
+
+# Appunti teorici
+
+Per migliorare l'accuratezza bisgona capire ancora meglio come funziona il pre-processing. Ogni passaggio, sia un po' dal punto di vista teorico che dal punto di vista pratico delle funzioni che vengono usate. 
+
--- parse-recepeit/tesseractOCR/tesseractOne.py
+++ parse-recepeit/tesseractOCR/tesseractOne.py
@@ -2,11 +2,23 @@
 from PIL import Image
 import cv2
 import pytesseract
+import numpy as np
+
+def preprocess_for_tesseract(image_path):
+    img = cv2.imread(image_path, 0)
+    img = cv2.resize(img, None, fx=4, fy=4, interpolation=cv2.INTER_CUBIC) # Il fattore di moltiplicazione dell'immagine è un x2
+    blur = cv2.GaussianBlur(img, (5, 5), 0)
+    thresh = cv2.adaptiveThreshold(
+        blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+        cv2.THRESH_BINARY_INV, 11, 2
+    ) # questo in caso il testo sia nero su sfondo bianco, (Binarizzazione)
+    kernel = np.ones((3, 3), np.uint8) 
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2) # rimuovi il rumore all'immagine
+    return opening
 
 def main():
-    # L'immagine giustamente deve essere pre-elaborata
+    processed_img = preprocess_for_tesseract("../images/scontrinoFake.png")
+    print(pytesseract.image_to_string(processed_img, lang='ita'))
     
-    print(pytesseract.image_to_string(Image.open("../images/scontrinoFake.png"),lang='ita'))
-
 if __name__ == "__main__":
     main()   
